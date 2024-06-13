5a15f6118f894fa2fe9cf38c9c2ff93470d242e6
Petrov Timofei (TP5) <timapetrov9@gmail.com>
2024-06-13 10:39:50 +0300

    Summer practice CGSG 2024 TP5 shaders for material aded.

--- 02-platon/bin/shaders/default/frag.glsl
+++ 02-platon/bin/shaders/default/frag.glsl
@@ -6,6 +6,17 @@ out vec4 OutColor;
 in vec3 DrawPos;
 in vec3 DrawNormal;
 
+uniform u_material {
+    vec4 Ka4;
+    vec4 Kd4;
+    vec4 KsPh;
+};
+
+#define Ka Ka4.xyz
+#define Kd Kd4.xyz
+#define Ks KsPh.xyz
+#define Ph KsPh.w
+
 uniform float Time;
 
 void main(void) {
@@ -16,12 +27,13 @@ void main(void) {
 
     float k = dot(L, normalize(N));
 
-    vec3 color = k * vec3(0, 0.7f, 0.6f);
+    vec3 color = k * Kd;//vec3(0.0f, 0.7f, 0.7f);
     vec3 R, V = vec3(0, 0, -1);
 
     R = reflect(V, N);
-    color += vec3(0.2f) * max(0.01f, pow(dot(R, L), 10.0f));
+    color += Ks * max(0.01f, pow(dot(R, L), Ph));
 
+    //OutColor = vec4(Ka, 1.0f);
     OutColor = vec4(color, 1.0f);
     //OutColor = vec4(N, 1.0);
 }
